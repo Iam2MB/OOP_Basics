acf7e6704367681e95ee03cb8cf022640a8f4356
Iam2MB <mirzano05@gmail.com>
2022-08-29 00:03:49 +0300

    DZ_1

---
+++ DZ_1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DZ_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Диаграммы UML");
+        }
+    }
+}
