310c4765179db0acf44262ee88a9b0a8eec4a366
Iam2MB <mirzano05@gmail.com>
2022-09-05 18:07:47 +0300

    DZ_3

---
+++ DZ_3_2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace DZ_3_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Reverse(Console.ReadLine());
+
+            Console.WriteLine(text);
+        }
+
+        static string Reverse(string text)
+        {
+            char[] reverse = text.ToCharArray();
+            Array.Reverse(reverse);
+            text = new string(reverse);
+
+            return text;
+        }
+    }
+}
